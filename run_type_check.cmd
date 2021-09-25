17606266ff6069f810ad3213262dfe9b9849c9e8
Jarrod <contact@jarrodmackay.ca>
2021-09-24 23:36:46 -0700

    fix script paths

--- run_type_check.cmd
+++ run_type_check.cmd
@@ -1,14 +1,13 @@
 @echo off
-%appdata%\..\Local\Programs\Python\Python38\Scripts\mypy Code/json_file.py
-%appdata%\..\Local\Programs\Python\Python38\Scripts\mypy Code/utf8_file.py
-%appdata%\..\Local\Programs\Python\Python38\Scripts\mypy Code/debug.py
 
-%appdata%\..\Local\Programs\Python\Python38\Scripts\mypy Code/accounting_objects.py
-%appdata%\..\Local\Programs\Python\Python38\Scripts\mypy Code/accounting.py
-%appdata%\..\Local\Programs\Python\Python38\Scripts\mypy Code/csv_importing.py
-
-%appdata%\..\Local\Programs\Python\Python38\Scripts\mypy Code/stocked_up.py
-%appdata%\..\Local\Programs\Python\Python38\Scripts\mypy Code/import_csv_data.py
-%appdata%\..\Local\Programs\Python\Python38\Scripts\mypy Code/map_spending_accounts.py
+mypy Code/json_file.py
+mypy Code/utf8_file.py
+mypy Code/debug.py
+mypy Code/accounting_objects.py
+mypy Code/accounting.py
+mypy Code/csv_importing.py
+mypy Code/stocked_up.py
+mypy Code/import_csv_data.py
+mypy Code/map_spending_accounts.py
 
 rmdir /s /q .mypy_cache
